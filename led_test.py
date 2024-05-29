e05098f578b6100ae070ca0c91ece0deffb06fce
Kenzhebek Taniyev <kenzhebektanyev@gmail.com>
2024-05-29 17:01:13 +0600

    led test

--- led_test.py
+++ led_test.py
@@ -2,26 +2,18 @@ import Jetson.GPIO as GPIO
 import time
 
 # Pin Definitions
-input_pin = 17  # BCM pin 17 (GPIO 17) connected to the button
-output_pin = 18  # BCM pin 18 (GPIO 18) connected to the LED
+output_pin = 19  # BCM pin 19 (physical pin 35) connected to the LED
 
-# Set up the GPIO channels
+# Set up the GPIO channel
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.LOW)
 
 try:
-    print("Press the button to light up the LED")
-    while True:
-        # Check if the button is pressed
-        if GPIO.input(input_pin) == GPIO.HIGH:
-            print("Button pressed!")
-            GPIO.output(output_pin, GPIO.HIGH)  # Turn on LED
-        else:
-            GPIO.output(output_pin, GPIO.LOW)  # Turn off LED
-        
-        # Small delay to prevent excessive CPU usage
-        time.sleep(0.1)
+    print("Lighting up the LED for 2 seconds")
+    GPIO.output(output_pin, GPIO.HIGH)  # Turn on LED
+    time.sleep(2)  # Keep the LED on for 2 seconds
+    GPIO.output(output_pin, GPIO.LOW)  # Turn off LED
+    print("LED turned off")
 
 except KeyboardInterrupt:
     print("Exiting gracefully")
